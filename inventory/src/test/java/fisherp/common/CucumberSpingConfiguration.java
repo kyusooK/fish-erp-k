c582d987107c6e70848bd16acaf245fe610569ee
kyusooK <rbtn110@uengine.org>
2023-10-23 07:19:35 +0000

    commit

--- inventory/src/test/java/fisherp/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package fisherp.common;
-
-import fisherp.InventoryApplication;
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { InventoryApplication.class })
-public class CucumberSpingConfiguration {}
